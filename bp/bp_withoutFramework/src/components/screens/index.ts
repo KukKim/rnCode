cb075198132f83718ac397b6285af19b30873c4a
KukKim <kukkim89@gmail.com>
2024-11-18 17:13:24 +0900

    add bp_withoutFramework

---
+++ bp/bp_withoutFramework/src/components/screens/index.ts
@@ -0,0 +1,4 @@
+import HomeScreen from './home';
+import OptionScreen from './option';
+
+export {HomeScreen, OptionScreen};
---
+++ bp/bp_withoutFramework/src/components/screens/option/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const OptionScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>OptionScreen</Text>
+    </View>
+  );
+};
+
+export default OptionScreen;
